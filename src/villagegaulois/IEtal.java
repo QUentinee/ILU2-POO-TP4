8a04bdd44de5680a432286178fd07a707bd16c86
Quentin Heudé <que.heude@gmail.com>
2024-04-07 19:42:32 +0200

    javance javabnce

--- src/villagegaulois/IEtal.java
+++ src/villagegaulois/IEtal.java
@@ -3,7 +3,6 @@ package villagegaulois;
 import personnages.Gaulois;
 
 public interface IEtal {
-	
 	Gaulois getVendeur();
 	double donnerPrix();
 	int contientProduit(String produit, int quantiteSouhaitee);
